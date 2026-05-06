fe2cb25b71e15c893e2fa8d6db1eb1be8d3a8f5a
Pink <isariya@rs.co.th>
2026-05-06 22:39:57 +0700

    Update redis

--- client/rq_client.py
+++ client/rq_client.py
@@ -1,9 +1,16 @@
 from redis import Redis
-from rq import Queue
+from rq import Worker, Queue
 
-queue = Queue(
-    connection=Redis(
-        host="valkey",
-        port=6379
-    )
+redis_conn = Redis(
+    host="valkey",
+    port=6379
 )
+
+queue = Queue(connection=redis_conn)
+
+worker = Worker(
+    [queue],
+    connection=redis_conn
+)
+
+worker.work()
